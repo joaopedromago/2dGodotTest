50a8696a6c3729bb0f00765af00b532f904eb619
João Mago <zaconou@gmail.com>
2024-01-08 13:31:23 -0300

    chore: format documents

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,22 +5,25 @@ var velocity = Vector2.ZERO
 var isGrounded = false
 var hasDoubleJump = false
 
+
 func _ready():
 	pass
+
 
 func _process(delta):
 	var hasVelocityChanges = false
 	var current_velocity = self.get_linear_velocity()
-	
-	var newXVelocity = velocity.x * speed	
+
+	var newXVelocity = velocity.x * speed
 	if newXVelocity != current_velocity.x:
 		current_velocity.x = newXVelocity
 		hasVelocityChanges = true
-		
+
 	if hasVelocityChanges:
 		self.set_linear_velocity(current_velocity)
-	
+
 	deathTest()
+
 
 func _input(event):
 	if event is InputEventKey:
@@ -32,16 +35,19 @@ func _input(event):
 			jump()
 		elif event.is_action_released("ui_left") or event.is_action_released("ui_right"):
 			velocity.x = 0
-	
+
+
 func _physics_process(delta):
 	var collisionObject = move_and_collide(velocity * speed * delta)
 	if collisionObject and collisionObject.get_collider().name:
 		isGrounded = true
-		
+
+
 func deathTest():
 	if ceil(self.global_position.y) >= 1000:
 		var menuScene = "res://scenes/menu.tscn"
 		get_tree().change_scene_to_file(menuScene)
+
 
 func jump():
 	if isGrounded:
@@ -51,10 +57,11 @@ func jump():
 	elif hasDoubleJump:
 		performJump()
 		hasDoubleJump = false
-	
+
+
 func performJump():
 	var input_vector = Vector2.ZERO
-	input_vector.y = -2;
+	input_vector.y = -2
 	var velocity = input_vector * speed
 	self.linear_velocity = velocity
 	self.set_linear_velocity(velocity)
